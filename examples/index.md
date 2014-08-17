4cb5f2913acc76ec8728af27e7e2ecc2ae976dbe
airyland <airyland@qq.com>
2014-08-17 23:00:33 +0800

    first version

--- examples/index.md
+++ examples/index.md
@@ -4,8 +4,42 @@
 
 ## Normal usage
 
-````javascript
-seajs.use('index', function(smartNav) {
+````iframe
+<style>
+    #box {
+        width:900px;
+        height:800px;
+        background:red;
+    }
 
+    #footer {
+        width:100%;
+        height:500px;
+        background:green;
+    }
+
+    #target {
+        width:200px;
+        height:250px;
+        background:blue;
+        position:fixed;
+        top:20px;
+        right:10px;
+    }
+
+</style>
+<div id="box"></div>
+<div id="footer"></div>
+
+<div id="target">
+    I am sidebar
+</div>
+<script>
+seajs.use('index', function(smartNav) {
+    new smartNav({
+        target:'#target',
+        column:'#box'
+    });
 });
+</script>
 ````
--- index.js
+++ index.js
@@ -1,3 +1,81 @@
-var smartNav;
+var $ = require('jquery'),
+    Eventor = require('eventor'),
+    $window = $(window),
+    $doc = $(document);
 
-module.exports = smartNav;
+
+var SmartNav = function(option) {
+    var $target = $(option.target);
+    // get top
+    var oTop = option.top || parseInt($target.css('top'), 10);
+    var sticky = option.sticky || false;
+
+    var $column = $(option.column),
+        columnOffset = option.columnOffset || 0;
+
+    var fixSidebar = function(onresize) {
+        // 屏幕宽度
+        var width = $(window).width();
+        var getRight = function() {
+            return 10;
+        };
+        var absoluteTop = $column.offset().top + columnOffset + $column.height();
+        var smallestOffset = sticky ? $target.offset().top : oTop + $target.height();
+        var middleOffset = $column.offset().top + columnOffset + $column.height() - $target.height() - 0;
+        var biggestOffset = $doc.height() - $window.height();
+
+        var handleBar = function() {
+            var top = $window.scrollTop();
+            if (top <= smallestOffset) {
+                if (!option.sticky) {
+                    $target.css({
+                        position: 'fixed',
+                        top: oTop,
+                        right: getRight()
+                    });
+                } else {
+                    // not support sticky now
+                }
+            } else if (top > smallestOffset && top < middleOffset) {
+                if ($target.css('position') === 'absolute') {
+                    $target.css({
+                        position: 'fixed',
+                        top: oTop,
+                        right: getRight()
+                    });
+                } else {
+                    $target.css({
+                        position: 'fixed',
+                        top: oTop,
+                        right: getRight()
+                    });
+                }
+            } else if (top > middleOffset) {
+                $target.css({
+                    position: 'absolute',
+                    right: getRight(),
+                    top: absoluteTop - $target.height()
+                });
+            }
+        };
+
+        if (onresize) {
+            handleBar();
+        }
+
+        $window.scroll(handleBar);
+    }
+
+    setTimeout(function() {
+        fixSidebar();
+    }, 1000);
+    $window.resize(function() {
+        fixSidebar(true);
+    });
+    setTimeout(function() {
+        $(window).trigger('scroll');
+    }, 1000)
+};
+
+Eventor.mixTo(SmartNav);
+module.exports = SmartNav;
