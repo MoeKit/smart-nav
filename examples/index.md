430be27c1e4b90172630e959d15dcb97039d76de
airyland <airyland@qq.com>
2014-08-18 23:49:21 +0800

    add bottom style support

--- examples/index.md
+++ examples/index.md
@@ -43,3 +43,44 @@ seajs.use('index', function(smartNav) {
 });
 </script>
 ````
+
+
+````iframe
+<style>
+    #box {
+        width:900px;
+        height:800px;
+        background:red;
+    }
+
+    #footer {
+        width:100%;
+        height:500px;
+        background:green;
+    }
+
+    #target {
+        width:200px;
+        height:250px;
+        background:blue;
+        position:fixed;
+        bottom:100px;
+        right:20px;
+    }
+
+</style>
+<div id="box"></div>
+<div id="footer"></div>
+
+<div id="target">
+    I am sidebar
+</div>
+<script>
+seajs.use('index', function(smartNav) {
+    new smartNav({
+        target:'#target',
+        column:'#box'
+    });
+});
+</script>
+````
--- index.js
+++ index.js
@@ -3,14 +3,17 @@ var $ = require('jquery'),
     $window = $(window),
     $doc = $(document);
 
-
 var SmartNav = function(option) {
-    var $target = $(option.target);
+    var $target = $(option.target),
+        targetHeight = $target.height();
+    this.mode = option.mode || $target.css('top') !== 'auto' ? 'top' : 'bottom';
     // get top
-    var oTop = option.top || parseInt($target.css('top'), 10);
+    var oTop = option.top || this.mode === 'top' ? parseInt($target.css('top'), 10) : ($window.height() - parseInt($target.css('bottom'), 10) - targetHeight);
+    var oriTop = $target.css('top');
+    var oriBottom = parseInt($target.css('bottom'), 10);
     var sticky = option.sticky || false;
     var right = option.right || parseInt($target.css('right'), 10);
-
+    var _this = this;
     var $column = $(option.column),
         columnOffset = option.columnOffset || 0;
 
@@ -24,15 +27,33 @@ var SmartNav = function(option) {
         var smallestOffset = sticky ? $target.offset().top : oTop + $target.height();
         var middleOffset = $column.offset().top + columnOffset + $column.height() - $target.height() - oTop;
         var biggestOffset = $doc.height() - $window.height();
-
+        var setOriginalStyle = function() {
+            $target.css({
+                top: oriTop,
+                bottom: oriBottom
+            });
+        };
         var handleBar = function() {
             var top = $window.scrollTop();
-            if (top <= smallestOffset) {
-                if (!option.sticky) {
+            if (_this.mode === 'bottom') {
+                var offset = $(window).height() - ($column.height() - ($window.scrollTop() - $column.offset().top));
+                if (offset < 100) {
+                    setOriginalStyle();
+                } else {
                     $target.css({
                         position: 'fixed',
-                        top: oTop
+                        bottom: offset
                     });
+                }
+                return;
+            }
+            if (top <= smallestOffset) {
+                if (!option.sticky) {
+
+                    $target.css({
+                        position: 'fixed',
+                    });
+                    setOriginalStyle();
                 } else {
                     // not support sticky now
                 }
@@ -40,14 +61,13 @@ var SmartNav = function(option) {
                 if ($target.css('position') === 'absolute') {
                     $target.css({
                         position: 'fixed',
-                        top: oTop
                     });
                 } else {
                     $target.css({
                         position: 'fixed',
-                        top: oTop
                     });
                 }
+                setOriginalStyle();
             } else if (top > middleOffset) {
                 $target.css({
                     position: 'absolute',
